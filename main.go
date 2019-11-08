272d3b831e467aeda03381080f97fd26b8a9ca7f
Pedro J. Ortiz <pedro@pjortiz.com>
2019-11-08 11:13:19 +0100

    added changes to support new languages

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ import (
 	"unicode/utf8"
 )
 
-const langsFastText = "af als am an ar arz as ast av az azb ba bar bcl be bg bh bn bo bpy br bs bxr ca cbk ce ceb ckb co cs cv cy da de diq dsb dty dv el eml en eo es et eu fa fi fr frr fy ga gd gl gn gom gu gv he hi hif hr hsb ht hu hy ia id ie ilo io is it ja jbo jv ka kk km kn ko krc ku kv kw ky la lb lez li lmo lo lrc lt lv mai mg mhr min mk ml mn mr mrj ms mt mwl my myv mzn nah nap nds ne new nl nn no oc or os pa pam pfl pl pms pnb ps pt qu rm ro ru rue sa sah sc scn sco sd sh si sk sl so sq sr su sv sw ta te tg th tk tl tr tt tyv ug uk ur uz vec vep vi vls vo wa war wuu xal xmf yi yo yue zh"
+const langsFastText = "af als am an ar arz as ast av az azb ba bar bcl be bg bh bn bo bpy br bs bxr ca cbk ce ceb ckb co cs cv cy da de diq dsb dty dv el eml en eo es et eu fa fi fr frr fy ga gd gl gn gom gu gv he hi hif hr hsb ht hu hy ia id ie ilo io is it ja jbo jv ka kk km kn ko krc ku kv kw ky la lb lez li lmo lo lrc lt lv mai mg mhr min mk ml mn mr mrj ms mt mwl my myv mzn nah nap nds ne new nl nn no oc or os pa pam pfl pl pms pnb ps pt qu rm ro ru rue sa sah sc scn sco sd sh si sk sl so sq sr su sv sw ta te tg th tk tl tr tt tyv ug uk ur uz vec vep vi vls vo wa war wuu xal xmf yi yo yue zh arabizi naija noise"
 
 type warcRecord struct {
 	header string
@@ -80,7 +80,7 @@ func main() {
 	go clasifyLines(data, files)
 
 	var wg sync.WaitGroup
-	maxGoroutines := 20
+	maxGoroutines := 5
 	guard := make(chan struct{}, maxGoroutines)
 	for path := range paths {
 		wg.Add(1)
@@ -232,7 +232,7 @@ func readWarcRecord(in *bufio.Reader) (warcRecord, error) {
 
 func clasifyLines(data <-chan string, files chan<- pair) {
 	var wg sync.WaitGroup
-	maxGoroutines := 10
+	maxGoroutines := 1
 	guard := make(chan struct{}, maxGoroutines)
 	for path := range data {
 		wg.Add(1)
@@ -268,7 +268,7 @@ func clasifyLines(data <-chan string, files chan<- pair) {
 			}
 			cleanWriter := bufio.NewWriter(clean)
 
-			cmd := exec.Command("fastText/fasttext", "predict-prob", "fastText/lid.176.bin", "-")
+			cmd := exec.Command("fastText/fasttext", "predict-prob", "fastText/model_fasttex_JOB_ID201526_1b782_40k_noise1_minn2_maxn5_Add_arabizi0_quantize1.bin", "-")
 
 			stdin, err := cmd.StdinPipe()
 			cmd.Stdout = tags
